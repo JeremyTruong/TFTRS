cfadb6ac002b286e6e538db5e0641a9a5b820e06
JeremyTruong <jeremytruong@hotmail.com>
2024-06-11 22:18:14 -0400

    Added compare button and handlers for comparison page

--- frontend/src/components/ComparisonPage/index.js
+++ frontend/src/components/ComparisonPage/index.js
@@ -5,7 +5,27 @@ class ComparisonPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            summonerOne: "",
+            summonerTwo: ""
         }
+    }
+
+    handleSummonerOneChange = (e) => {
+        this.setState({
+            summonerOne: e.target.value
+        })
+    }
+
+    handleSummonerTwoChange = (e) => {
+        this.setState({
+            summonerTwo: e.target.value
+        })
+        console.log(this.state.summonerTwo);
+    }
+
+    handleCompareButtonClick = (e) => {
+        this.setState({
+        })
     }
 
     render() {
@@ -24,11 +44,22 @@ class ComparisonPage extends React.Component {
             spacing={2}
             >
                 <Grid item>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={this.handleSummonerOneChange}/>
                 </Grid>
                 <Grid item>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={this.handleSummonerTwoChange}/>
                 </Grid>
+            </Grid>
+
+            <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            marginY={4}
+            >
+                    <Button variant="contained" onClick={this.handleCompareButtonClick}>Compare</Button>
             </Grid>
             
             
